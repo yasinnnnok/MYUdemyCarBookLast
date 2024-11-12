9e07de6f7dc3936e6375d049e52ce87a59e9fd7a
yasinok <yasinok@subu.edu.tr>
2024-11-12 14:55:36 +0300

    Area oluşturuldu. Banner Listeleme yapıldı.

--- Front2/CarBook.Dto/BannerDtos/ResultBannerDto.cs
+++ Front2/CarBook.Dto/BannerDtos/ResultBannerDto.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace UdemyCarBook.Dto.BannerDtos
+namespace CarBook.Dto.BannerDtos
 {
     public class ResultBannerDto
     {
---
+++ Front2/CarBook.WebUI/Areas/Admin/Controllers/AdminBannerController.cs
@@ -0,0 +1,34 @@
+﻿using CarBook.Dto.BannerDtos;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace CarBook.WebUI.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class AdminBannerController : Controller
+    {
+
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public AdminBannerController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7060/api/Banners");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultBannerDto>>(jsonData);
+                return View(values);
+
+            }
+
+            return View();
+        }
+
+    }
+}
