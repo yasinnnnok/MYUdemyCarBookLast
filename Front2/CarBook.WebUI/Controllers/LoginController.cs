4bc4d5b04d348bc950a10f943cb03a4e16646929
yasinok <yasinok@subu.edu.tr>
2024-11-19 16:49:06 +0300

    Login sayfa tasarımı yapıldı.

---
+++ Front2/CarBook.WebUI/Controllers/LoginController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CarBook.WebUI.Controllers
+{
+    public class LoginController : Controller
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+        public LoginController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+
+
+    }
+}
