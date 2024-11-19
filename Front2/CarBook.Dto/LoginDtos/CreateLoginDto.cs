1f5a4a46e6e441da7929d85e17db6fc9b6a485d1
yasinok <yasinok@subu.edu.tr>
2024-11-19 16:39:43 +0300

    Register işlemi yapıldı.

--- Front2/CarBook.Dto/LoginDtos/CreateLoginDto.cs
+++ Front2/CarBook.Dto/LoginDtos/CreateLoginDto.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace UdemyCarBook.Dto.LoginDtos
+﻿namespace CarBook.Dto.LoginDtos
 {
     public class CreateLoginDto
     {
